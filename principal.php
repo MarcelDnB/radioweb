26653c0fe5c4d85a2e3153fc39912e6494a63630
MarcelDnB <44347173+MarcelDnB@users.noreply.github.com>
2019-06-12 20:13:30 +0200

    El navbar se va a quedar arriba, +
    pondre las opciones del menu dentro del div del reproductor, si hace falta mover el reproductor al lado izquierdo borrando eso, y en el lado derecho poner las opciones junto al boton de loguearse. MOVER EL REPRODUCTOR UN POCO A LA DERECHA Y EN ESE HUECO IRIA EL LOGO, EN EL ESPACIO SOBRANTE DE LA DERECHA PONER LAS OPCIONES JUNTO AL BOTON DE LOGUEARSE AL FINAL DEL TODO.

--- principal.php
+++ principal.php
@@ -1,5 +1,3 @@
-<!DOCTYPE html>
-<html>
 <head>
   <meta charset="UTF-8">
   <title>Music app - TweenMax</title>
@@ -10,141 +8,16 @@
 
   
 </head>
-<body>
-    
-</body>
-</html>
 
-<!-- navbar -->
-     <!--<div class="navbar navbar-default navbar-fixed-top" role="navigation">
-      <div class="container">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="#">Project name</a>
-        </div>
-        <div class="navbar-collapse collapse">
-          <ul class="nav navbar-nav">
-            <li class="active"><a href="#">Home</a></li>
-            <li><a href="#about">About</a></li>
-            <li><a href="#contact">Contact</a></li>
-          </ul>
-        </div>
-      </div>
-  </div>
-/navbar -->
+
+
+
 
 <body>
+<?php 
+include("player.php");
 
+?>
 
 
 </body>
-
-<footer>
-<div class="mini-player">
-    <div class="track_info_wrapper">
-      <div class="track_info">
-        <div class="thumb"></div>
-        <div class="info">
-          <div class="title">Friday Comes</div>
-          <div class="artist">Early</div>
-        </div>
-      </div>
-    </div>
-    <div class="mini-player_btn_wrapper"><i class="btn-prev fa fa-step-backward" aria-hidden="true"></i>
-      <div class="btn-switch"><i class="btn-play fa fa-play" aria-hidden="true"></i><i class="btn-pause fa fa-pause" aria-hidden="true"></i></div><i class="btn-next fa fa-step-forward" aria-hidden="true"></i><i class="btn-open-player fa fa-list" aria-hidden="true"></i>
-    </div>
-  </div>
-  <div class="dim"></div>
-  <div class="player" id="player">
-    <div class="playback_wrapper">
-      <div class="playback_blur"></div>
-      <div class="playback_thumb"></div>
-      <div class="playback_info">
-        <div class="title">Friday Comes</div>
-        <div class="artist">Early</div>
-      </div>
-      <div class="playback_btn_wrapper"><i class="btn-prev fa fa-step-backward" aria-hidden="true"></i>
-        <div class="btn-switch"><i class="btn-play fa fa-play" aria-hidden="true"></i><i class="btn-pause fa fa-pause" aria-hidden="true"></i></div><i class="btn-next fa fa-step-forward" aria-hidden="true"></i>
-      </div>
-      <div class="playback_timeline">
-        <div class="playback_timeline_start-time">00:31</div>
-        <div class="playback_timeline_slider">
-          <div class="slider_base"></div>
-          <div class="slider_progress"></div>
-          <div class="slider_handle"></div>
-        </div>
-        <div class="playback_timeline_end-time">03:11</div>
-      </div>
-    </div>
-    <div class="list_wrapper">
-      <ul class="list"> 
-        <li class="list_item selected"> 
-          <div class="thumb"> </div>
-          <div class="info"> 
-            <div class="title">Friday Comes</div>
-            <div class="artist">Early</div>
-          </div>
-        </li>
-        <li class="list_item"> 
-          <div class="thumb"> </div>
-          <div class="info"> 
-            <div class="title">Friday Comes</div>
-            <div class="artist">Early</div>
-          </div>
-        </li>
-        <li class="list_item"> 
-          <div class="thumb"> </div>
-          <div class="info"> 
-            <div class="title">Friday Comes</div>
-            <div class="artist">Early</div>
-          </div>
-        </li>
-        <li class="list_item"> 
-          <div class="thumb"> </div>
-          <div class="info"> 
-            <div class="title">Friday Comes</div>
-            <div class="artist">Early</div>
-          </div>
-        </li>
-        <li class="list_item"> 
-          <div class="thumb"> </div>
-          <div class="info"> 
-            <div class="title">Friday Comes</div>
-            <div class="artist">Early</div>
-          </div>
-        </li>
-        <li class="list_item"> 
-          <div class="thumb"> </div>
-          <div class="info"> 
-            <div class="title">Friday Comes</div>
-            <div class="artist">Early</div>
-          </div>
-        </li>
-        <li class="list_item"> 
-          <div class="thumb"> </div>
-          <div class="info"> 
-            <div class="title">Friday Comes</div>
-            <div class="artist">Early</div>
-          </div>
-        </li>
-        <li class="list_item"> 
-          <div class="thumb"> </div>
-          <div class="info"> 
-            <div class="title">Friday Comes</div>
-            <div class="artist">Early</div>
-          </div>
-        </li>
-      </ul>
-    </div>
-  </div>
-</div>
-  <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.0/jquery.min.js'></script>
-<script src='https://cdnjs.cloudflare.com/ajax/libs/gsap/1.19.0/TweenMax.min.js'></script>
-<script src='https://cdnjs.cloudflare.com/ajax/libs/gsap/1.19.0/TimelineMax.min.js'></script>
-    <script  src="js/script.js"></script>
-</footer>
