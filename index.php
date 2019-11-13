4e71bd6d480fd6e91b979ed5cabaedee47e90e03
MarcelDnB <44347173+MarcelDnB@users.noreply.github.com>
2019-11-13 20:08:14 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,3 @@
 <?php
     header("Location: principal.php");
-/* Esto es la pagina de inicio de la pagina, la cual redirigirá a la página principal de la web en concreto
-En este caso llevará a la página general que se verá cuando cualquiera entre en la web. Es decir en donde saldrá el botón de play de la radio
-en concreto, dependiendo de la url, en este caso de la general lleva a la pagina general. Para entrar en la página de administrar es necesario conocer una url especial
-que solo los administradores sabran. */
 ?>
